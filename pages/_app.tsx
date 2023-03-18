96a5c9de775078763fc684663bbc5eed37db20d0
Nate Mueller <inathan.m@gmail.com>
2023-03-18 11:45:05 -0700

    title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
+  appName: "OG Potheads",
   chains,
 });
 
